37de820ee871961e0985398275f7ffd4f7c9a4c8
rondi201 <domnin.danil@mail.ru>
2023-04-26 16:39:22 +0700

    :snake: Add LinearConcat

--- opr/models/fusion.py
+++ opr/models/fusion.py
@@ -30,3 +30,20 @@ class Concat(FusionModule):
             concat_data.append(value.reshape(batch, -1))
         fusion_global_descriptor = torch.concat(concat_data, dim=1)
         return fusion_global_descriptor
+
+class LinearConcat(Concat):
+    """Concatenation module for 'image' and 'cloud' modalities."""
+
+    def __init__(self, in_features, out_features) -> None:
+        """Concatenation module for 'image' and 'cloud' modalities."""
+        super().__init__()
+        self.head = torch.nn.Sequential(
+            torch.nn.Linear(in_features, out_features),
+            torch.nn.BatchNorm1d(out_features)
+        )
+
+    def forward(self, data: Dict[str, Union[Tensor, ME.SparseTensor]]) -> Tensor:  # noqa: D102
+        fusion_global_descriptor = super().forward(data)
+        fusion_global_descriptor = self.head(fusion_global_descriptor)
+
+        return fusion_global_descriptor
